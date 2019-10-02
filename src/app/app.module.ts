c9398ee332e6d91bb7becf61476bcc46cd2c481d
Midhun Pk <pkr.midhun@gmail.com>
2019-10-02 13:15:43 +0530

    Added new component navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { GridComponent } from './components/grid/grid.component';
 import { VisualizerComponent } from './components/visualizer/visualizer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     GridComponent,
-    VisualizerComponent
+    VisualizerComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
