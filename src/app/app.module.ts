0b6e3bcaa06580f745e1a66aecf09e40ca493f62
Midhun Pk <pkr.midhun@gmail.com>
2019-10-02 19:58:02 +0530

    Removed angular-material and cdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,12 +7,6 @@ import { HomeComponent } from './components/home/home.component';
 import { GridComponent } from './components/grid/grid.component';
 import { VisualizerComponent } from './components/visualizer/visualizer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
-import {
-  MatIconModule,
-  MatButtonModule,
-  MatTooltipModule
-} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -25,11 +18,7 @@ import {
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    MatIconModule,
-    MatButtonModule,
-    MatTooltipModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
