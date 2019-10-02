fc7d7467f0dfdac7df978575ad0786bde599ee2c
Midhun Pk <pkr.midhun@gmail.com>
2019-10-02 19:20:08 +0530

    Added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +8,12 @@ import { HomeComponent } from './components/home/home.component';
 import { GridComponent } from './components/grid/grid.component';
 import { VisualizerComponent } from './components/visualizer/visualizer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+
+import {
+  MatIconModule,
+  MatButtonModule,
+  MatTooltipModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -18,7 +25,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
