3f6654a282f504cac60e402289b456b6c9d4937a
Midhun Pk <pkr.midhun@gmail.com>
2019-10-08 19:26:34 +0530

    Added submenu in right menu container

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -8,6 +8,28 @@ import { SidebarService } from 'src/app/services/sidebar.service';
 })
 export class SidebarComponent implements OnInit {
   active: boolean;
+  menus = [{
+    name: 'Algorithms',
+    value: 'algorithms',
+    subMenu: [{
+      name: 'Breadth First Search',
+      value: 'bfs',
+    }, {
+      name: 'Depth First Search',
+      value: 'dfs',
+    }, {
+      name: 'Dijikstra\'s',
+      value: 'dijikstra',
+    }, {
+      name: 'A* Search',
+      value: 'astar',
+    }]
+  }, {
+    name: 'Mazes',
+    value: 'mazes'
+  }];
+  selectedMenu = {};
+  subMenus = [];
 
   constructor(private sidebarService: SidebarService) { }
 
@@ -17,9 +39,11 @@ export class SidebarComponent implements OnInit {
   onCloseSideBarClick() {
     this.active = false;
     this.sidebarService.hideSideBar();
+    this.selectedMenu = {};
   }
 
-  onMenuClick() {
+  onMenuClick(menu) {
+    this.selectedMenu = menu;
     this.active = true;
   }
 
