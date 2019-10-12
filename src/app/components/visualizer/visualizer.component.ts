cde9471623d625e2c2413e9c7aba5b3a61a8ebc6
Midhun Pk <pkr.midhun@gmail.com>
2019-10-12 20:36:44 +0530

    Removed buttons on the left panel

--- src/app/components/visualizer/visualizer.component.ts
+++ src/app/components/visualizer/visualizer.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { PathFinderService } from 'src/app/services/path-finder.service';
-import { GridAnimationService } from 'src/app/services/grid-animation.service';
-import { SidebarService } from 'src/app/services/sidebar.service';
-import { Algorithm } from 'src/app/models/algorithm.model';
-import { GridService } from 'src/app/services/grid.service';
-import { Grid } from 'src/app/models/grid.model';
 
 @Component({
   selector: 'app-visualizer',
@@ -12,41 +6,11 @@ import { Grid } from 'src/app/models/grid.model';
   styleUrls: ['./visualizer.component.scss']
 })
 export class VisualizerComponent implements OnInit {
-  algorithm: Algorithm;
-  grid: Grid;
 
   constructor(
-    private pathFinderService: PathFinderService,
-    private gridAnimationService: GridAnimationService,
-    private sidebarService: SidebarService,
-    private gridService: GridService
   ) { }
 
   ngOnInit() {
-    this.pathFinderService.getAlgorithm().subscribe(algorithm => this.algorithm = algorithm);
-    this.gridService.getGrid().subscribe(grid => this.grid = grid);
   }
 
-  onClearWalls() {
-    this.gridService.clearWalls();
-  }
-
-  onVisualize() {
-    if (this.algorithm) {
-      const success = this.pathFinderService.runAlgorithm(this.grid);
-      this.gridAnimationService.animateAlgorithm(this.grid);
-    }
-  }
-
-  onResetButtonClick() {
-    this.gridService.reset();
-  }
-
-  onClearAnimationButtonClick() {
-    this.gridAnimationService.clearAnimation();
-  }
-
-  onSettingsButtonClick() {
-    this.sidebarService.showSideBar();
-  }
 }
