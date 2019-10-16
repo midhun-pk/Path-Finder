497dc6510123c77ef01b76db7273671ef67d6a38
Midhun Pk <pkr.midhun@gmail.com>
2019-10-16 22:22:18 +0530

    Added basic methods for recursive backtracking maze generation algorith

--- src/app/services/maze-generation-algorithm.service.ts
+++ src/app/services/maze-generation-algorithm.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Grid } from '../models/grid.model';
+import { Node } from '../models/node.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,19 @@ import { Injectable } from '@angular/core';
 export class MazeGenerationAlgorithmService {
 
   constructor() { }
+
+  recursiveBacktracking(grid: Grid) {
+    const numberOfNodeVisited = 0;
+    const stack: Node[] = [];
+
+    stack.push(grid.gridArray[0][0]);
+    while (stack.length > 0) {
+      const currentNode = stack.pop();
+    }
+  }
+
+  getNeighbors(currentNode: Node) {
+    const neighbors: string[] = [];
+    const coordinates = currentNode.id.split('-');
+  }
 }
