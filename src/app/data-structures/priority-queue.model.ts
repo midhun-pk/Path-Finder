a661237e8160ab9e1cf98935283357663b81d124
Midhun Pk <pkr.midhun@gmail.com>
2019-10-19 23:56:37 +0530

    Added insert and decrease priority methods

--- src/app/data-structures/priority-queue.model.ts
+++ src/app/data-structures/priority-queue.model.ts
@@ -1,7 +1,7 @@
 export class PriorityQueue {
     size = 0;
-    array = [];
-    pos = {};
+    array: [string | number, number][] = [];
+    position = {};
 
     isEmpty(): boolean {
         return this.size === 0;
@@ -15,14 +15,18 @@ export class PriorityQueue {
         return 2 * index + 2;
     }
 
+    parent(index: number): number {
+        return Math.floor(index / 2);
+    }
+
     min_heapify(index: number) {
         const leftChild = this.left(index);
         const rightChild = this.right(index);
         let smallest = index;
-        if (leftChild < this.size && this.array[leftChild] < this.array[index]) {
+        if (leftChild < this.size && this.array[leftChild][1] < this.array[index][1]) {
             smallest = leftChild;
         }
-        if (rightChild < this.size && this.array[rightChild] < this.array[smallest]) {
+        if (rightChild < this.size && this.array[rightChild][1] < this.array[smallest][1]) {
             smallest = rightChild;
         }
         if (smallest !== index) {
@@ -32,8 +36,27 @@ export class PriorityQueue {
     }
 
     swap(index1: number, index2: number) {
+        this.position[this.array[index1][0]] = index2;
+        this.position[this.array[index2][0]] = index1;
         const temp = this.array[index1];
         this.array[index1] = this.array[index2];
         this.array[index2] = temp;
+    }
+
+    insert(node: string | number, priority: number) {
+        this.position[node] = this.size;
+        this.size += 1;
+        this.array.push([node, Infinity]);
+        this.decreasePriority(node, Infinity, priority);
+    }
+
+    decreasePriority(node: string | number, oldPriority: number, newPriority: number) {
+        let index = this.position[node];
+        const parentIndex = this.parent(index);
+        this.array[index] = [node, newPriority];
+        while (index > 0 && this.array[parentIndex][1] > this.array[index][1]) {
+            this.swap(index, parentIndex);
+            index = parentIndex;
+        }
     }
 }
