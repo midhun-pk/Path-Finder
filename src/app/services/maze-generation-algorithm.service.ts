5684fdb8ceaf2b3ed2a4a8f32377974694314211
Midhun Pk <pkr.midhun@gmail.com>
2019-10-20 01:35:03 +0530

    Break if visitednodes length equals max visitable length

--- src/app/services/maze-generation-algorithm.service.ts
+++ src/app/services/maze-generation-algorithm.service.ts
@@ -15,13 +15,11 @@ export class MazeGenerationAlgorithmService {
   addBorderWalls(grid: Grid) {
     grid.nodesToAnimate.push(grid.gridArray[0][0]);
     grid.gridArray[0][0].visited = true;
-    grid.nodesToAnimate.push(grid.gridArray[0][0]);
     let i = 0;
     let j = 1;
     while (!(i === 0 && j === 0)) {
       grid.nodesToAnimate.push(grid.gridArray[i][j]);
       grid.gridArray[i][j].visited = true;
-      grid.nodesToAnimate.push(grid.gridArray[i][j]);
       if (i === 0 && j < grid.columns - 1) {
         j += 1;
       } else if (j === grid.columns - 1 && i < grid.rows - 1) {
@@ -38,11 +36,12 @@ export class MazeGenerationAlgorithmService {
     const specialNodes = ['start', 'target'];
     grid.nodesToAnimate = [];
     this.addBorderWalls(grid);
-    const numberOfNodesVisited = 0;
     const stack: Node[] = [];
     stack.push(grid.gridArray[1][1]);
-    grid.gridArray[0][0].visited = true;
-    while (stack.length > 0) {
+    grid.gridArray[1][1].visited = true;
+    let numberOfNodesVisited = 1;
+    const maxNumberOfNodesVisitable = ((grid.rows - 1) / 2) * ((grid.columns - 1) / 2);
+    while (stack.length > 0 && numberOfNodesVisited !== maxNumberOfNodesVisitable) {
       const currentNode = stack[stack.length - 1];
       const neighbors = this.gridService.getNeighbors(currentNode.id, [], 1, true);
       if (neighbors.length > 0) {
@@ -50,6 +49,7 @@ export class MazeGenerationAlgorithmService {
         const connectingNodeIds = this.gridService.getConnectingNodes(currentNode.id, nextNodeId);
         connectingNodeIds.forEach(nodeId => grid.nodes[nodeId].visited = true);
         grid.nodes[nextNodeId].visited = true;
+        numberOfNodesVisited += 1;
         stack.push(grid.nodes[nextNodeId]);
       } else {
         stack.pop();
