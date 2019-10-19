0992ff5021a8475a808c35dbf091c1fc1437073d
Midhun Pk <pkr.midhun@gmail.com>
2019-10-19 22:29:34 +0530

    Added min_heapify to priority queue

--- src/app/data-structures/priority-queue.model.ts
+++ src/app/data-structures/priority-queue.model.ts
@@ -14,4 +14,26 @@ export class PriorityQueue {
     right(index: number): number {
         return 2 * index + 2;
     }
+
+    min_heapify(index: number) {
+        const leftChild = this.left(index);
+        const rightChild = this.right(index);
+        let smallest = index;
+        if (leftChild < this.size && this.array[leftChild] < this.array[index]) {
+            smallest = leftChild;
+        }
+        if (rightChild < this.size && this.array[rightChild] < this.array[smallest]) {
+            smallest = rightChild;
+        }
+        if (smallest !== index) {
+            this.swap(smallest, index);
+            this.min_heapify(smallest);
+        }
+    }
+
+    swap(index1: number, index2: number) {
+        const temp = this.array[index1];
+        this.array[index1] = this.array[index2];
+        this.array[index2] = temp;
+    }
 }
