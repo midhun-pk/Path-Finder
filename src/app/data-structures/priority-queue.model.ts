08ec45131faf6729a798de306ae357a18b758883
Midhun Pk <pkr.midhun@gmail.com>
2019-10-19 22:13:42 +0530

    Added functions to return the left and right index of the child of a node in a tree

--- src/app/data-structures/priority-queue.model.ts
+++ src/app/data-structures/priority-queue.model.ts
@@ -6,4 +6,12 @@ export class PriorityQueue {
     isEmpty(): boolean {
         return this.size === 0;
     }
+
+    left(index: number): number {
+        return 2 * index + 1;
+    }
+
+    right(index: number): number {
+        return 2 * index + 2;
+    }
 }
